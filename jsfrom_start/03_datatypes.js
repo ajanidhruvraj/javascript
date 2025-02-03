92c973fa10a2a70acf98ffae7211401b6aab1f8a
Dhruvraj ajani <166250079+ajanidhruvraj@users.noreply.github.com>
2025-02-03 09:44:57 +0000

    03_js

---
+++ jsfrom_start/03_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newernversion
+
+//alert(3+3)//wecant do this because this is node js
+
+// number ==> 2 to power 53
+// bigint
+// string ==> ""
+// boolean ==> true/false
+// null ==> standalone value
+// undefined
+// symbol ==> uniqe
+// object
+let account4 =9;
+
+console.log(typeof account4);
