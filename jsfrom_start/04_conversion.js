ec4571b19c45863852abbc251de637db61f8ee46
Dhruvraj ajani <166250079+ajanidhruvraj@users.noreply.github.com>
2025-02-03 13:48:34 +0000

    04_js

--- jsfrom_start/04_conversion.js
+++ jsfrom_start/04_conversion.js
@@ -5,4 +5,8 @@ let num = Number(score);
 console.log(typeof num);//number,number,number
 console.log(num);//1==>true,0==>false
 
-
+let fname = "dj";
+let lname = " ajani"
+console.log(fname+lname);
+console.log("1"+2+2);
+console.log(1+2+"2");   
