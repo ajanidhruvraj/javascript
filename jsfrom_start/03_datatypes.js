03b8bb5e0ba14cd2f21e9f9a72d5c48c2ae398bc
Dhruvraj ajani <166250079+ajanidhruvraj@users.noreply.github.com>
2025-02-03 14:00:33 +0000

    03_js

--- jsfrom_start/03_datatypes.js
+++ jsfrom_start/03_datatypes.js
@@ -15,3 +15,11 @@ let account4;
 console.log(typeof account4);
 console.log(typeof undefined);//undefine
 console.log(typeof null);//object
+
+// primitive 7
+//string,number,null,boolean,undefined,symbol,BigInt
+
+//reference type/non premetive
+//Array,objects,functions
+
+//dynamic or static ?find
